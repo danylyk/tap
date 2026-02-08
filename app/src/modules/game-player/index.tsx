196de007fd1bdaaf084b06f70354968bff9b9200
Danylyk Vitalii <danylyk.vitalii@gmail.com>
2026-02-08 15:23:59 +0200

    feat: attach camera to player

--- app/src/modules/game-player/index.tsx
+++ app/src/modules/game-player/index.tsx
@@ -1,8 +1,51 @@
+import {useFrame, useThree} from "@react-three/fiber/native";
+import {useEffect, useRef} from "react";
+import {Group} from "three";
+
 export default function Module() {
+  const camera = useThree((state) => {
+    return state.camera;
+  });
+
+  const player = useRef<Group>(null);
+  const root = useRef<Group>(null);
+
+  useEffect(() => {
+    if (!root.current) {
+      return;
+    }
+
+    root.current.add(camera);
+  }, [camera]);
+
+  useFrame((state) => {
+    if (!root.current) {
+      return;
+    }
+
+    if (!player.current) {
+      return;
+    }
+
+    const t = Math.min(state.clock.elapsedTime, 20) / 20;
+
+    const x = 0;
+    const y = 0;
+    const z = t * 270;
+
+    player.current.position.set(x, y, z);
+    root.current.position.set((x + z) / 2, 0, (x + z) / 2);
+  });
+
   return (
-    <mesh position={[15, 15, 15]}>
-      <boxGeometry args={[30, 30, 30]} />
-      <meshStandardMaterial color={0xbaf455} />
-    </mesh>
+    <>
+      <group ref={root} />
+      <group ref={player}>
+        <mesh position={[15, 15, 15]}>
+          <boxGeometry args={[30, 30, 30]} />
+          <meshStandardMaterial color={0xbaf455} />
+        </mesh>
+      </group>
+    </>
   );
 }
