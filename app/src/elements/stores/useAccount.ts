3625fd4e8772d76e9dffe276e2eb577a3ff831f5
Danylyk Vitalii <danylyk.vitalii@gmail.com>
2026-03-17 21:32:39 +0200

    feat: add persistence to useAccount store

--- app/src/elements/stores/useAccount.ts
+++ app/src/elements/stores/useAccount.ts
@@ -1,69 +1,86 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {createJSONStorage, persist} from "expo-zustand-persist";
+import {Platform} from "react-native";
 import {create} from "zustand";
 
-export default create<{
-  attempts: {
-    [id: string]: {
+export default create(
+  persist<{
+    attempts: {
+      [id: string]: {
+        marks: {
+          position: {
+            x: number;
+            z: number;
+          };
+        }[];
+      };
+    };
+    setAttempt: (payload: {
+      id: string;
       marks: {
         position: {
           x: number;
           z: number;
         };
       }[];
-    };
-  };
-  setAttempt: (payload: {
-    id: string;
-    marks: {
+    }) => void;
+    addMark: (payload: {
+      id: string;
       position: {
         x: number;
         z: number;
       };
-    }[];
-  }) => void;
-  addMark: (payload: {
-    id: string;
-    position: {
-      x: number;
-      z: number;
-    };
-  }) => void;
-}>((set, get) => {
-  return {
-    attempts: {},
-    setAttempt: ({id, marks}) => {
-      set((state) => {
-        return {
-          attempts: {
-            ...state.attempts,
-            [id]: {
-              marks,
-            },
-          },
-        };
-      });
-    },
-    addMark: ({id, position}) => {
-      const attempt = get().attempts[id];
-
-      if (!attempt) {
-        return;
-      }
-
-      set((state) => {
-        return {
-          attempts: {
-            ...state.attempts,
-            [id]: {
-              marks: [
-                ...attempt.marks,
-                {
-                  position,
+    }) => void;
+  }>(
+    (set, get) => {
+      return {
+        attempts: {},
+        setAttempt: ({id, marks}) => {
+          set((state) => {
+            return {
+              attempts: {
+                ...state.attempts,
+                [id]: {
+                  marks,
                 },
-              ],
-            },
-          },
-        };
-      });
+              },
+            };
+          });
+        },
+        addMark: ({id, position}) => {
+          const attempt = get().attempts[id];
+
+          if (!attempt) {
+            return;
+          }
+
+          set((state) => {
+            return {
+              attempts: {
+                ...state.attempts,
+                [id]: {
+                  marks: [
+                    ...attempt.marks,
+                    {
+                      position,
+                    },
+                  ],
+                },
+              },
+            };
+          });
+        },
+      };
     },
-  };
-});
+    {
+      name: "account",
+      storage: createJSONStorage(() => {
+        if (Platform.OS === "web") {
+          return localStorage;
+        }
+
+        return AsyncStorage;
+      }),
+    },
+  ),
+);
